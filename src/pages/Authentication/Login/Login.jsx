5ddae411b5cc4439eeb070d309e85f9fc15b3911
AminulIslamorange <aminulislamorange@gmail.com>
2025-12-02 11:42:12 +0600

    login from added

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,10 +1,41 @@
+import { useForm } from "react-hook-form";
 
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit} = useForm()
+
+    const onSubmit = (data) => console.log(data)
+    
     return (
-        <div>
-            <h2>This is Login</h2>
-        </div>
+        <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+            <fieldset className="fieldset w-full max-w-xs mx-auto">
+                <label className="label font-bold text-base">Email</label>
+                <input
+                    type="email"
+                    className="input input-bordered w-full"
+                    placeholder="Email" {...register("email")}
+                />
+
+
+                <label className="label mt-3 font-bold text-base">Password</label>
+                <input
+                    type="password"
+                    className="input input-bordered w-full"
+                    placeholder="Password" {...register("password")}
+                />
+
+                <div className="mt-2">
+                    <a className="link link-hover">Forgot password?</a>
+                </div>
+
+                <button className="btn btn-neutral w-full mt-4 bg-[#CAEB66] text-black font-bold">
+                    Login
+                </button>
+            </fieldset>
+        </form>
+
     );
 };
 
