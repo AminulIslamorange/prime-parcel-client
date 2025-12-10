48b339904808510fe846f07edb4aceee9877d039
AminulIslamorange <aminulislamorange@gmail.com>
2025-12-10 20:27:56 +0600

    today all done

--- src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -1,6 +1,25 @@
-
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+const formatDate=(iso)=>new Date(iso).toLocaleString();
 
 const PaymentHistory = () => {
+    const {user}=useAuth();
+    const axiosSecure=useAxiosSecure();
+
+    const {isPending,data:payments=[]}=useQuery({
+        queryKey:['payments',user.email],
+        queryFn:async()=>{
+            const res=await axiosSecure.get(`/payments?email=${user.email}`)
+            return res.data;
+        }
+    })
+    if(isPending){
+        return '...Loading'
+
+}
+
+
     return (
         <div>
             
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -10,6 +10,8 @@ import SendParcel from "../pages/SendParcel/SendParcel";
 import DashBoardLayout from "../layouts/DashBoardLayout";
 import MyParcels from "../pages/Dashboard/MyParcels/MyParcels";
 import Payment from "../pages/Dashboard/Payment/Payment";
+import PaymentHistory from "../pages/Dashboard/PaymentHistory/PaymentHistory";
+import TrackParcel from "../pages/Dashboard/TrackParcel/TrackParcel";
 
 export const router = createBrowserRouter([
   {
@@ -62,6 +64,14 @@ export const router = createBrowserRouter([
         {
           path:'payment/:parcelId',
           element:<Payment></Payment>
+        },
+        {
+          path:'paymentHistory',
+          element:<PaymentHistory></PaymentHistory>
+        },
+        {
+          path:'track',
+          element:<TrackParcel></TrackParcel>
         }
       ]
   }
